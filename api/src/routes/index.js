3ff475610e3b92ecbe3ce900ca93b2fe4abbd3e6
serrsua <serrsua.88@gmail.com>
2023-11-29 00:38:21 -0300

    fix index de routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,5 +1,11 @@
 const { Router } = require('express');
 
+const gamesRouter = require("./gamesRouter");
+const genresRouter = require("./genresRouter");
+
 const router = Router();
+
+router.use("/games", gamesRouter);
+router.use("/genres", genresRouter);
 
 module.exports = router;
